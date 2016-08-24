a9d2cbef5260439739b362ad72f25950b7bf71c8
null_logic <U6016812@U6016812-TPL-B.ten.thomsonreuters.com>
2016-08-25 00:08:26 +0800

    test

---
+++ iSurvey_live/iSurveyTest/com/isurvey/conn/db/ConnectionName.java
@@ -0,0 +1,17 @@
+package com.isurvey.conn.db;
+
+public class ConnectionName {
+	
+	private String connectionName;
+
+	public String getConnectionName() {
+		return connectionName;
+	}
+  
+	protected void setConnectionName(String connectionName) {
+		this.connectionName = "iperform_survey_db_test";
+	}
+	
+	
+	
+}
